617a3de0900eb1274426ef92efb91c1a7973c3fb
cesartc <cesartarrillocelada@gmail.com>
2013-05-29 14:33:13 -0500

    para probar vistar mediante routes.php

---
+++ application/views/borrar.blade.php
@@ -0,0 +1,7 @@
+<head>
+<script src="js/a.js" type="text/javascript"></script>
+</head>
+<body>
+<a href="#" onclick="javascript:alert('dasdas');">LINk</a><br>
+<a href="#" onclick="prob()">DESDE /js</a>
+</body>
---
+++ public/js/a.js
@@ -0,0 +1,4 @@
+function prob()
+{
+	alert("lalalal");
+}
